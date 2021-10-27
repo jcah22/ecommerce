41baec77a920b6f8f3bfadb03127795e0b4401cc
julscode <jcah549@gmail.com>
2021-10-27 15:50:56 -0500

    creando clases de modelo del proyecto

---
+++ src/main/java/com/julscode/ecommerce/EcommerceApplication.java
@@ -0,0 +1,15 @@
+package com.julscode.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+//@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@SpringBootApplication
+public class EcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceApplication.class, args);
+	}
+
+}
